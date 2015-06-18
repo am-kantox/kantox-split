df19f47656f948b9e5126bdf891e97734bb3769a
Aleksei Matiushkin <aleksei.matiushkin@kantox.com>
2015-06-18 18:10:42 +0200

    Deep to_h :v:.

--- lib/kantox/split.rb
+++ lib/kantox/split.rb
@@ -20,7 +20,9 @@ module Kantox
         e.reflections.values
       end
       configure_embedded :attributes
-      configure_schild %i(name caption label).map{ |field| "respond_to?(:#{field}) && #{field}" }.join(' || ') + " || ''"
+      configure_schild %w(name caption label)
+                        .flat_map { |e| [ e, "#{self.class.name.singularize.underscore}_#{e}"] }
+                        .map { |field| "respond_to?(:#{field}) && #{field}" }.join(' || ') + " || ''"
 
       ##########################################################################
       #### Graph for classes
@@ -32,14 +34,21 @@ module Kantox
         end
         def vertices
           edges.map do |edge|
+            next [edge.name, ::ActiveRecord::Base] if edge.options[:polymorphic]
+
             while edge && edge.options[:through] do
               edge = edges.detect { |e| e.name == edge.options[:through] }
             end
-            edge && [edge.name, (edge.options[:class_name] || edge.name.to_s).singularize.camelize.constantize]
+
+            edge_singular = (edge.options[:class_name] || edge.name.to_s).singularize.camelize
+            edge_singular += 's' if edge_singular[-2..-1] == 'es' # Fuck Rails
+            edge && [edge.name, edge_singular] # (edge_singular.constantize rescue edge_singular)]
           end.compact.to_h
         end
-        def to_h
-          vertices
+        def to_h levels = 0, collected = []
+          vertices.map do |k, v|
+            [k, !collected.include?(v) && levels > 0 && v.respond_to?(:to_h) ? { v => v.to_h(levels - 1, collected << v) } : v]
+          end.to_h
         end
       end
     end
